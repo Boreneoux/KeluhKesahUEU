5464b1641726f4e703792b9882d4b3dc39d3010e
Boreneoux <ichlasul.ap@gmail.com>
2023-01-29 21:33:27 +0700

    initialization

---
+++ backend/resources/views/mantap.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Goks</title>
+</head>
+<body style="background-color:azure">
+    <div style="flex: ">
+        <h1>Hello, World!</h1>
+        <h1>icul disini bos</h1>
+        <h1>Laravel with NGINX in Docker DONE!</h1>
+        <p>Keluh Kesah UEU adalah sebuah website yang dibuat oleh  Fasilkom UEU Kampus Tangerang yang dapat dipergunakan sebagai wadah kritik dan saran selama masa perkuliahan di Universitas Esa Unggul.</p>
+    </div>
+</body>
+</html>
